b17a37da6dff38bccffd4ce08262c5f369727890
noreakram21 <noreakram22@gmail.com>
2018-10-17 15:17:23 +0800

    commit again

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 const path = require('path');
-const publicPath =path.join(__dirname, '../public');
 const express = require('express');
+
+const publicPath =path.join(__dirname, '../public');
+let app = express();
+
 const port = process.env.PORT || 3000;
 //app.listen(port, () => { console.log(`App listening on port ${port}!`); })
-let app = express();
+
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+app.listen(port, () => {
 	console.log(`Server is up on ${port}`);
 });
